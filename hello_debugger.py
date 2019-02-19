46536c5f0b84f5ea8fbbe2c38c0254e158562321
Cameron Taylor <axis7818@gmail.com>
2019-02-19 12:52:11 -0800

    initial python program with debugging and task

---
+++ hello_debugger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from hello import say_hi
+
+
+def parse_args():
+    parser = ArgumentParser(
+        prog="Hello Debugger",
+        description="A Simple hello world app to test vscode debugging",
+    )
+    parser.add_argument("-n", "--name", help="who to say hi to", required=True)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    say_hi(args.name)
